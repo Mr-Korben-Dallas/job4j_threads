52fe7d4bfa4260f4bcc8b0f8092af859cc5f3010
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-06-24 14:50:28 +0400

    [#1019]:  - implemented logic for thread interrupting;

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsoleProgress implements Runnable{
+    @Override
+    public void run() {
+        int step = 0;
+        ArrayList<Character> characters = new ArrayList<>(
+                List.of('-', '\\', '|', '/')
+        );
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                System.out.print("\rLoading: " + characters.get(step));
+                Thread.sleep(500);
+                step = (step < 3) ? ++step : 0;
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000);
+        progress.interrupt();
+    }
+}
