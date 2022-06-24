042822bc741d5e0cfef903a5ac61a8f45fcdb93d
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-06-24 14:58:39 +0400

    [#267413]:  - added interrupt in catch block;

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -24,7 +24,7 @@ public class ConsoleProgress implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(10000);
+        Thread.sleep(1000);
         progress.interrupt();
     }
 }
