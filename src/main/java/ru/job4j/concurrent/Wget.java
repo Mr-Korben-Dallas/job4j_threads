6c786b8cea28fb94a5faac3e34eb4ae7f9b3c86d
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-06-27 11:19:18 +0400

    [#144271]:  - implemented logic for file downloading;

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,21 +1,62 @@
 package ru.job4j.concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        String filename = "downloaded-file";
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(filename)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long before;
+            long after;
+            long referenceTime;
+            while ((before = System.currentTimeMillis()) > 0
+                    && (bytesRead = in.read(dataBuffer, 0, 1024)) != -1
+            ) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                after = System.currentTimeMillis();
+                referenceTime = after - before;
+                if (referenceTime < speed) {
                     try {
-                        System.out.println("Loading is started ... ");
-                        for (int index = 0; index <= 100; index++) {
-                            System.out.print("\rLoaded : " + index  + "%");
-                            Thread.sleep(1000);
-                        }
-                        System.out.println("\nDone.");
+                        Thread.sleep(speed - referenceTime);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-        );
-        thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length != 2) {
+            throw new InterruptedException("Enter URL and speed parameters");
+        }
+        if (!args[0].contains("https://")) {
+            throw new InterruptedException("URL must have: https://");
+        }
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        if (speed < 0) {
+            throw new InterruptedException("Speed param invalid");
+        }
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
